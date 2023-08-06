4c3efd359fff647cb65eb14fe50db0d71d4f87f8
Dane <dane.mckillop@gmail.com>
2023-08-06 23:39:12 +1000

    include custom marker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
+import iconURL from "./icons/placeholder.png"
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
 
 const position = [51.505, -0.09];
+
+const baseMarker = L.icon({
+  iconUrl: iconURL,
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
 
 export default function App() {
   return (
@@ -13,7 +21,7 @@ export default function App() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="..."
       />
-      <Marker position={position}>
+      <Marker position={position} icon={baseMarker}>
         <Popup>
           A marker with a popup.
         </Popup>
