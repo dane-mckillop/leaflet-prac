9927f51a13fc4b568cf74fe7b978c8178e38921c
Dane <dane.mckillop@gmail.com>
2023-08-10 23:13:23 +1000

    Searchbar is updating test correctly. Fix div styling.

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,10 @@
-import {useState, useRef} from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 export default function SearchBar(props) {
-    const [innerSearch, setInnerSearch] = useState("");
-    const [test, setTest] = useState("");
     const { position, setPosition } = props;
+    const [innerSearch, setInnerSearch] = useState("");
+    const [isMounted, setMounted] = useState(false);
+    const [test, setTest] = useState("");
     const searchInputRef = useRef(null);
 
     const handleKeyDown = (event) => {
@@ -12,24 +13,38 @@ export default function SearchBar(props) {
         }
     };
 
+    //onClick handler rerenders endlessly without this function
+    const handleSearch = () => {
+        setTest(innerSearch);
+    };
+
+    useEffect(() => {
+        if (isMounted) {
+            console.log(test);
+        } else {
+            setMounted(true);
+        }
+    }, [test]);
+
+
     return (
         <div className="search-bar">
-        <input
-            aria-labelledby="search-button"
-            name="search"
-            id="search"
-            type="search"
-            value={innerSearch}
-            onChange={(e) => setInnerSearch(e.target.value)}
-            onKeyDown={handleKeyDown}
-            ref={searchInputRef}
-        />
-        <button
-            className="search-button"
-            //onClick={setTest(innerSearch)} //UPDATE TO SOME API CALL
-        >
-            Search
-        </button>
-    </div>
+            <input
+                aria-labelledby="search-button"
+                name="search"
+                id="search"
+                type="search"
+                value={innerSearch}
+                onChange={(e) => setInnerSearch(e.target.value)}
+                onKeyDown={handleKeyDown}
+                ref={searchInputRef}
+            />
+            <button
+                className="search-button"
+                onClick={handleSearch} //CHANGE LATER TO SOME API CALL
+            >
+                Search
+            </button>
+        </div>
     )
 }
