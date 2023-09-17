ec7c6ece8889df834430885ad1edaee6af8bc809
Dane <dane.mckillop@gmail.com>
2023-09-17 23:34:54 +1000

    Added the FetchNews method. Add functionality and implement.

---
+++ src/api/FetchNews.js
@@ -0,0 +1,11 @@
+//Import the base news API url here
+
+//CHANGE THE BELOW SUCH THAT IT IMPORTS NEWS ON SEARCH
+export default function FetchNews(country) {
+    //let url = API_URL + `/some/changes`;
+
+    //return fetch(url).
+        //.then(response => {
+            //return response.json()})
+        //.then(news => { return news});
+}
