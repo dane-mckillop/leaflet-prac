5e4585215cd4c1bcccd2e4592c6212415c5ef923
Dane <dane.mckillop@gmail.com>
2023-10-16 00:00:11 +1000

    Add comment to fetchNews

--- src/api/FetchNews.js
+++ src/api/FetchNews.js
@@ -3,6 +3,13 @@ import API_URL from '../components/config.js';
 //CHANGE THE BELOW SUCH THAT IT IMPORTS NEWS ON SEARCH
 //Add country codes in JSON.
 //Referenced in: 
+/**
+ * Fetchs the news response object from news.api with a
+ * pre-structured api_url fetch request.
+ * 
+ * @param {*} country 
+ * @returns 
+ */
 export default function FetchNews(country) {
     let url = API_URL + `?country=` + country.code;
 
